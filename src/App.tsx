cc5151b355c8b507641ac9b08fe47ad4776a7552
danieldagerom <danielprimo9@gmail.com>
2023-03-05 02:01:15 -0300

    Cria campo de item e botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Header } from './components/Header';
+import { ItemBox } from './components/ItemBox';
+import styles from './App.module.css'
 import './global.css'
 
 export function App() {
     return (
         <div>
             <Header />
+            <div className={styles.wrapper}>
+                <ItemBox />
+                <button> 
+                    Criar + 
+                </button>
+            </div>
         </div>
     );
 }
---
+++ src/components/ItemBox.tsx
@@ -0,0 +1,9 @@
+import styles from './ItemBox.module.css'
+
+export function ItemBox () {
+    return (
+        <div className={styles.itemBox}>
+            
+        </div>
+    );
+}
